3304ec5ae361e6e9a4bd74fc2f4a844b2ffa4003
Nkosr <62732850+Nkosr@users.noreply.github.com>
2020-04-19 00:26:16 -0400

    Final_Project
    Know Your City

---
+++ widgets/Trails/Widget.js
@@ -0,0 +1,8 @@
+<!-- TRAILFORKS WIDGET START -->
+<div class="TrailforksWidgetMap" data-w="800px" data-h="400px" data-rid="8372" data-activitytype="1" data-maptype="osm" data-trailstyle="difficulty" data-controls="1" data-list="0" data-dml="1" data-layers="labels,poi,polygon,directory,region" data-z="" data-lat="" data-lon="" data-hideunsanctioned="0"></div>
+<a href="https://www.trailforks.com/region/sudbury/">Sudbury</a> on <a href="https://www.trailforks.com/">Trailforks.com</a>
+
+<script type="text/javascript">
+var script = document.createElement("script"); script.setAttribute("src", "https://es.pinkbike.org/ttl-86400/sprt/j/trailforks/widget.js"); document.getElementsByTagName("head")[0].appendChild(script); var widgetCheck = false;
+</script>
+<!-- TRAILFORKS WIDGET END -->
